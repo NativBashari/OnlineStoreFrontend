e3f308bac4fd4cd82197f3a911cf325df4d5cb20
NativBashari <nativbas2000@gmail.com>
2023-02-09 16:47:08 +0200

    RegistrationImplemented

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, NumberValueAccessor, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserRegister } from 'src/app/Models/userRegister.model';
 import { IsAdminService } from 'src/app/Services/Auth/isAdmin.service';
 import { User } from '../../Models/user.model';
 import { AuthService } from '../../Services/Auth/auth.Service';
@@ -36,9 +37,9 @@ export class LoginComponent implements OnInit {
   login: FormGroup = new FormGroup({
     userName: new FormControl('', [Validators.required]),
     password : new FormControl('', [Validators.required]),
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
-    telephone: new FormControl('', [Validators.required])
+    firstName: new FormControl('firstName', [Validators.required]),
+    lastName: new FormControl('lastName', [Validators.required]),
+    telephone: new FormControl('Phone number', [Validators.required])
   });
 
   switchToSignUp(){
@@ -47,26 +48,54 @@ export class LoginComponent implements OnInit {
 
 
   loginUser(){
-    if(this.login.valid){
-      let username = this.login.controls['userName'].value;
-      let password = this.login.controls['password'].value;
-      let user: User = {
-        username: username,
-        password: password
+    if(!this.isSignUp){
+      if(this.login.valid){
+        let username = this.login.controls['userName'].value;
+        let password = this.login.controls['password'].value;
+        let user: User = {
+          username: username,
+          password: password
+        }
+        this.authService.tryLogin(user).subscribe(res =>{
+          this.res = res as {token: string, isSuccess: boolean, message: string, isAdmin: boolean, userDetails: {firstName: string, lastName: string, userId: string}};
+          localStorage.setItem("jwt", this.res.token);
+          localStorage.setItem("Role", this.res.isAdmin? "Admin" : "User");
+          localStorage.setItem("fName", this.res.userDetails.firstName),
+          localStorage.setItem("lName", this.res.userDetails.lastName),
+          localStorage.setItem("id" , this.res.userDetails.userId);
+          this.router.navigate([""]);
+        },
+        error => {
+          this.errorMessage = error.error.message;
+        }
+        )
       }
-      this.authService.tryLogin(user).subscribe(res =>{
-        this.res = res as {token: string, isSuccess: boolean, message: string, isAdmin: boolean, userDetails: {firstName: string, lastName: string, userId: string}};
-        localStorage.setItem("jwt", this.res.token);
-        localStorage.setItem("Role", this.res.isAdmin? "Admin" : "User");
-        localStorage.setItem("fName", this.res.userDetails.firstName),
-        localStorage.setItem("lName", this.res.userDetails.lastName),
-        localStorage.setItem("id" , this.res.userDetails.userId);
-        this.router.navigate([""]);
-      },
-      error => {
-        this.errorMessage = error.error.message;
-      }
-      )
     }
+    else{
+      if(this.login.valid){
+        let username = this.login.controls['userName'].value;
+        let password = this.login.controls['password'].value;
+        let firstName = this.login.controls['firstName'].value;
+        let lastName = this.login.controls['lastName'].value;
+        let telephone = this.login.controls['telephone'].value;
+        let user: UserRegister = {
+          username: username,
+          password: password,
+          firstName: firstName,
+          lastName: lastName,
+          telephone : telephone
+        }
+        this.authService.tryRegister(user).subscribe(res=> {
+          this.errorMessage = "You register succesfully, now you can login";
+          this.isSignUp = false;
+        },
+        err => {
+          this.errorMessage = "Username already exist."
+        }
+        );
+        this.login.setValue({userName: '', password: '', firstName: '', lastName: '', telephone: '' })
+
+    }
+  }
   }
 }
